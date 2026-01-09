4b2bec008e91a9bba2c7852fbd4304cec28eb9ac
Elsa <elsacpestana@gmail.com>
2026-01-09 16:02:58 -0400

    Time: 0 ms (100%), Space: 97.4 MB (24.52%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        if (prices.size() < 2) return 0;
+
+        int min = prices[0];
+        int maxGanancia = 0;
+
+        for(int i= 1; i< prices.size(); i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }
+
+            int G = prices[i] - min;
+
+            if(G > maxGanancia){
+                maxGanancia = G;
+            }
+        }
+
+        return maxGanancia;
+        
+    }
+};
